169f1d54603df0a88669f931e38054716d8bcd9a
Martin-WMM <Martin-WMM@users.noreply.github.com>
2026-09-19 04:24:29 +0000

    deploy: f5812b2a24a3709c7583ae16e3e937f1e3120961

---
+++ assets/swimlanesDiagram-TC7HE7FX-2vfWnvLD.js
@@ -0,0 +1,8 @@
+import{c as r,s as e}from"./flowDiagram-KWPJA3E3-BYlgjOhb.js";import{_ as a}from"./index-DPyQzbvj.js";import"./chunk-5VM5RSS4-D4cjFFsO.js";import"./chunk-XXDRQBXY-Cypu1LA0.js";import"./chunk-WEXAMYUT-CaDyyPQU.js";import"./chunk-GWA4HPMP-B1hMW2kL.js";import"./channel-DEs-Lg74.js";var o=a(t=>`${e(t)}
+  .swimlane.cluster:not([data-color-id]) rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),s=o,f=r({styles:s});export{f as diagram};
